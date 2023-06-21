94f8b377eb648ab8a823b6bfebcfda313d735e18
Gianpaolo Pedroni <gianpaolodelarosa@gmail.com>
2023-06-21 18:07:18 +0100

    animation
    Animation for level select and go back button

--- KPIUnity/Assets/Level_Script.cs
+++ KPIUnity/Assets/Level_Script.cs
@@ -9,17 +9,48 @@ public class Level_Script : MonoBehaviour
     public Animator transition;
 
     public float transitiontime = 1f;
-    public void LoadLevel(string menu)
+    public void LoadLevelMenu(string menu)
     {
         
         
         StartCoroutine(LoadNextLevel(menu));
     }
+    public void LoadLevelSettings(string menu)
+    {
 
+
+        StartCoroutine(LoadNextSettings(menu));
+    }
+    public void LoadLevelSelecLevel(string menu)
+    {
+
+
+        StartCoroutine(LoadNextSelecLevel(menu));
+    }
     IEnumerator LoadNextLevel(string menu)
     {
         transition.SetTrigger("Start");
         
+        yield return new WaitForSeconds(transitiontime);
+
+        SceneManager.LoadScene(menu);
+
+
+    }
+    IEnumerator LoadNextSettings(string menu)
+    {
+        transition.SetTrigger("GoBack");
+
+        yield return new WaitForSeconds(transitiontime);
+
+        SceneManager.LoadScene(menu);
+
+
+    }
+    IEnumerator LoadNextSelecLevel(string menu)
+    {
+        transition.SetTrigger("GoBack");
+
         yield return new WaitForSeconds(transitiontime);
 
         SceneManager.LoadScene(menu);
